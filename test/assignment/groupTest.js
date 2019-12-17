ae628e932972aec04d68887da3c5eb17fe0d3694
Joonas Puumala <joonas.puumala@tuni.fi>
2019-12-17 18:21:40 +0000

    test file

---
+++ test/assignment/groupTest.js
@@ -0,0 +1,14 @@
+
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+let Game = require('../../public/js/gameFile.js');
+
+describe('gameFile tests',function(){
+  it('<what to test>', function(){
+
+  })
+
+});
